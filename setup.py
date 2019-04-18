241ff36b2e225c4ad2a67d0acc7d21530e912009
Wojciech Basiura <basiura.wojciech@gmail.com>
2019-04-19 00:46:39 +0200

    Fixed #1 - setup.py building library by calling makefile

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 from setuptools import setup, Distribution
+from setuptools.command.install import install as SetupToolsInstall
+from os import system
+
+class MyInstall(SetupToolsInstall):
+    def run(self):
+        system('make libmy_qsort.so')
+        SetupToolsInstall.run(self)
 
 class BinaryDistribution(Distribution):
     def has_ext_modules(foo):
         return True
 
 setup(
-    name="my_qsort",
-    version="0.1",
-    packages=["my_qsort"],
+    name='my_qsort',
+    version='0.1',
+    packages=['my_qsort'],
     package_data={
-        "my_qsort": ['libmy_qsort.so']
+        'my_qsort': ['libmy_qsort.so']
     },
+    cmdclass={'install': MyInstall},
     distclass=BinaryDistribution
 )
 
